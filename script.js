33af523691d233a7da40d0aebb866aa60c14783c
swimmingRiver <swimmingr@gmail.com>
2023-08-24 14:34:37 +0900

    add delete

--- script.js
+++ script.js
@@ -7,16 +7,20 @@ const LoadToDo =()=>{
     const localArrValue = JSON.parse(localArr);
     localArrValue?.map((i)=>arr.push(i));
     ShowTodoList();
-};
+};//localStorage에 있는 리스트 불러오기
 const ShowTodoList=()=>{
     list.innerHTML='';
-    arr.map((i)=>{
+    arr.map((value,index)=>{
     const listItem = document.createElement('li')
-    listItem.innerHTML=`<input type="checkbox"><p>${i}</p>`;
+    listItem.innerHTML=`<span key=${index}
+    <input type="checkbox">
+    <p>${value}</p>
+    <button>Edit</button>
+    <button onclick={DeleteToDo(${index})}>delete</button>
+    </span>`;
     list.appendChild(listItem);
  })
-
-}
+};//html로 저장한 값 보여주기
 
 const AddToDo=()=>{
     if(todo.value===""){
@@ -26,13 +30,15 @@ const AddToDo=()=>{
     localStorage.setItem('todoList',JSON.stringify(arr));
     ShowTodoList();
     todo.value = '';
-};
+};//입력값 리스트에 추가하기
 
-const DeleteToDo =()=>{
-
+const DeleteToDo =(index)=>{
+    arr.splice(index,1);
+    localStorage.setItem("todoList",JSON.stringify(arr));
+    ShowTodoList();
 }
 
 
 LoadToDo();
 add.addEventListener("click",AddToDo);
-// document.addEventListener("DOMContentLoaded",);
+
