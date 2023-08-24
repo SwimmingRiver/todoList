9f3ba321945aab3729248fa00e9d7baf7d4f6cad
swimmingRiver <swimmingr@gmail.com>
2023-08-24 18:10:46 +0900

    디자인 추가 및 에러 수정

--- script.js
+++ script.js
@@ -8,15 +8,15 @@ const LoadToDo =()=>{
     localArrValue?.map((i)=>arr.push(i));
     ShowTodoList();
 };//localStorage에 있는 리스트 불러오기
+
 const ShowTodoList=()=>{
-    
-    list.innerHTML='';
+        list.innerHTML='';
     arr.map((value,index)=>{
     const done = arr[index].done?'checked':"";
     const listItem = document.createElement('li')
     listItem.innerHTML=`<div id="todo-box" key=${index}>
-    <input type="checkbox" ${done} onchange="CheckToggle(${index})">
-    <p>${value.value}</p>
+    <input id="check-box" type="checkbox" ${done} onchange="CheckToggle(${index})">
+    <p id="todo-value">${value.value}</p>
     <button id="button-in-box" onclick={EditToDo(${index})}>
     <span class="material-symbols-outlined">
 edit
@@ -37,14 +37,20 @@ const CheckToggle =(index)=>{
     ShowTodoList();
 }
 
-const AddToDo=()=>{
-    if(todo.value===""){
+const AddToDo=(e)=>{
+    e.preventDefault();
+    const inputToDo = document.querySelector('#inputVal');
+    let inputToDoVal = inputToDo.value;
+    if(inputToDoVal===""){
         return alert('값을 입력해주세요');
      };
-    arr.push({value:todo.value,done:false});
+    if(inputToDoVal!==""){
+
+    arr.push({value:inputToDoVal,done:false});
     localStorage.setItem('todoList',JSON.stringify(arr));
     ShowTodoList();
-    todo.value = '';
+    inputToDo.value = '';
+    }
 };//입력값 리스트에 추가하기
 
 const DeleteToDo =(index)=>{
@@ -56,9 +62,12 @@ const DeleteToDo =(index)=>{
 };//삭제하기
 
 const EditToDo =(index)=>{
-    let edtied = prompt(`${arr[index].value}를 수정하시겠습니까?`,`${arr[index]}`);
+    let edtied = prompt(`${arr[index].value}를 수정하시겠습니까?`,`${arr[index].value}`);
+    if(!edtied){
+        return 0;
+    }
     if(edtied){
-    arr.splice(index,1,edtied);
+    arr.splice(index,1,{value:edtied,done:arr[index].done});
     localStorage.setItem("todoList",JSON.stringify(arr));
     ShowTodoList();}
 };//수정하기
