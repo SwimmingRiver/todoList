845569aa6051ed65527753e64a32a277e8add80e
swimmingRiver <swimmingr@gmail.com>
2023-08-24 16:50:52 +0900

    add check toggle

--- script.js
+++ script.js
@@ -9,31 +9,46 @@ const LoadToDo =()=>{
     ShowTodoList();
 };//localStorage에 있는 리스트 불러오기
 const ShowTodoList=()=>{
+    
     list.innerHTML='';
     arr.map((value,index)=>{
+    const done = arr[index].done?'checked':"";
     const listItem = document.createElement('li')
-    listItem.innerHTML=`<span key=${index}
-    <input type="checkbox">
-    <p>${value}</p>
-    <button onclick={EditToDo(${index})}>Edit</button>
-    <button onclick={DeleteToDo(${index})}>delete</button>
-    </span>`;
+    listItem.innerHTML=`<div id="todo-box" key=${index}>
+    <input type="checkbox" ${done} onchange="CheckToggle(${index})">
+    <p>${value.value}</p>
+    <button id="button-in-box" onclick={EditToDo(${index})}>
+    <span class="material-symbols-outlined">
+edit
+</span>
+    </button>
+    <button id="button-in-box" onclick={DeleteToDo(${index})}>
+    <span class="material-symbols-outlined">
+    close
+    </span></button>
+    </div>`;
     list.appendChild(listItem);
  })
 };//html로 저장한 값 보여주기
+
+const CheckToggle =(index)=>{
+    arr[index].done = !arr[index].done;
+    localStorage.setItem("todoList",JSON.stringify(arr));
+    ShowTodoList();
+}
 
 const AddToDo=()=>{
     if(todo.value===""){
         return alert('값을 입력해주세요');
      };
-    arr.push(todo.value);
+    arr.push({value:todo.value,done:false});
     localStorage.setItem('todoList',JSON.stringify(arr));
     ShowTodoList();
     todo.value = '';
 };//입력값 리스트에 추가하기
 
 const DeleteToDo =(index)=>{
-    let check = confirm(`${arr[index]}를 삭제하시겠습니까?`);
+    let check = confirm(`${arr[index].value}를 삭제하시겠습니까?`);
     if(check){
     arr.splice(index,1);
     localStorage.setItem("todoList",JSON.stringify(arr));
@@ -41,10 +56,11 @@ const DeleteToDo =(index)=>{
 };//삭제하기
 
 const EditToDo =(index)=>{
-    let edtied = prompt(`${arr[index]}를 수정하시겠습니까?`,`${arr[index]}`);
+    let edtied = prompt(`${arr[index].value}를 수정하시겠습니까?`,`${arr[index]}`);
+    if(edtied){
     arr.splice(index,1,edtied);
     localStorage.setItem("todoList",JSON.stringify(arr));
-    ShowTodoList();
+    ShowTodoList();}
 };//수정하기
 
 
